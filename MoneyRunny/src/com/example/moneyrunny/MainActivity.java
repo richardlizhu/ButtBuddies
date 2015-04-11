123a0fe9ba0e9757f496b80b8ed6abb7114c75ec
Richard Zhu <richardlizhu@gmail.com>
2015-04-11 13:50:39 -0400

    added some tabs

--- MoneyRunny/src/com/example/moneyrunny/MainActivity.java
+++ MoneyRunny/src/com/example/moneyrunny/MainActivity.java
@@ -68,6 +68,9 @@ public class MainActivity extends ActionBarActivity implements
 		case 3:
 			mTitle = getString(R.string.title_section3);
 			break;
+		case 4:
+			mTitle = getString(R.string.title_section4);
+			break;
 		}
 	}
 
